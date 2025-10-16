2bb9e73aff4ce096988aebc6c0934ad2976a4d50
Anastasiia <schili@outlook.com>
2025-10-16 16:35:06 +0300

    another token issue

--- controllers/signUp/logout.js
+++ controllers/signUp/logout.js
@@ -4,8 +4,8 @@ const logout = async (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "None",
     });
 
     res.json({
